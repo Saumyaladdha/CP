76d3602df38d1148fffcd67873880651c3837e62
Saumyaladdha <136463212+Saumyaladdha@users.noreply.github.com>
2024-10-10 20:38:27 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int x = 0;
+    
+    for (int i = 0; i < n; i++) {
+        string statement;
+        cin >> statement;
+        
+        if (statement.find("++") != string::npos) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    
+    cout << x << endl;
+    
+    return 0;
+}
